96db7ad11fd0e612dc47d91429a83646f73c347c
Yannick <yannick.radke@gmail.com>
2019-03-16 15:57:12 +0100

    deleted checkboxes (persons) and added a button with a popup-menu.

--- app/src/main/java/radke/yannick/android/emailmessage/MainActivity.java
+++ app/src/main/java/radke/yannick/android/emailmessage/MainActivity.java
@@ -1,15 +1,19 @@
 package radke.yannick.android.emailmessage;
 
-import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Buttons:
+        Button btnShowPeople = findViewById(R.id.btn_show_people);
+
+        // Person-choose:
+        btnShowPeople.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String[] items = {" Kira Schatzi", " Mama", " Papa", " Stefan B.", "Srefan S."};
+                final ArrayList itemsSelected = new ArrayList();
+
+                Dialog dialog;
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Personenwahl");
+                builder.setMultiChoiceItems(items, null, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedItemId, boolean isSelected) {
+                        if (isSelected) {
+                            itemsSelected.add(selectedItemId);
+                        } else if (itemsSelected.contains(selectedItemId)) {
+                            itemsSelected.remove(Integer.valueOf(selectedItemId));
+                        }
+                    }
+                })
+                        .setPositiveButton("Done!", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                //Your logic when OK button is clicked
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                Toast.makeText(MainActivity.this, "Abgebrochen", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                dialog = builder.create();
+                dialog.show();
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
